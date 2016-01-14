24ed6fd7a8ae066e88a362cb6726124d42c8fbd8
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2016-01-14 12:37:39 +0800

    support STS in oss client

--- test/sts.test.js
+++ test/sts.test.js
@@ -14,20 +14,45 @@
 
 var assert = require('assert');
 var sts = require('../').STS;
+var oss = require('..');
 var env = process.env;
 
 describe('test/sts.test.js', function () {
   var stsConfig = {
     accessKeyId: env.ALI_SDK_STS_ID,
     accessKeySecret: env.ALI_SDK_STS_SECRET,
-    roleArn: env.ALI_SDK_STS_ROLE
+    roleArn: env.ALI_SDK_STS_ROLE,
+    bucket: env.ALI_SDK_STS_BUCKET
   };
 
   describe('assumeRole()', function () {
     it('should assume role', function* () {
       var stsClient = sts(stsConfig);
       var result = yield* stsClient.assumeRole(stsConfig.roleArn);
-      assert.ok(result.Credentials.AccessKeyId);
+      assert.equal(result.res.status, 200);
+    })
+
+    it('should list objects using STS', function* () {
+      var stsClient = sts(stsConfig);
+      var result = yield* stsClient.assumeRole(stsConfig.roleArn);
+      assert.equal(result.res.status, 200);
+
+      var ossClient = oss({
+	region: 'oss-cn-hangzhou',
+	accessKeyId: result.credentials.AccessKeyId,
+	accessKeySecret: result.credentials.AccessKeySecret,
+	stsToken: result.credentials.SecurityToken,
+	bucket: stsConfig.bucket,
+      });
+
+      var result = yield ossClient.put('sts/hello', __filename);
+      assert.equal(result.res.status, 200);
+
+      var result = yield ossClient.list({
+	'max-keys': 10
+      });
+
+      assert.equal(result.res.status, 200);
     })
   });
 });
