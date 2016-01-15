6ba8cbb9ec5108bb3f0bf49e4131c22042c37f93
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2016-01-15 11:38:57 +0800

    add comments to browser.js

--- browser.js
+++ browser.js
@@ -1,4 +1,28 @@
+/*
+ * Export `OSS` as standalone lib which can be used in browser
+ *
+ * Use browserify:
+ *  browserify browser.js --standalone OSS > ali-oss-sdk.js
+ *
+ * @example
+ * <script type="text/javascript" src="ali-oss-sdk.js"></script>
+ * <script type="text/javascript">
+ *   var oss = new OSS(options);
+ *   OSS.co(function* () {
+ *     var result = oss.list({
+ *       'max-keys': 10
+ *     });
+ *     console.log(result);
+ *   }).then(function () {
+ *     // pass
+ *   }).catch(function (err) {
+ *     console.log(err);
+ *   });
+ * </script>
+ */
+
 var OSS = require('.');
 OSS.co = require('co');
+OSS.urllib = require('urllib');
 
 module.exports = OSS;
