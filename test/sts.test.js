f40614c823fe6b4562537a46738376cd696231d6
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2016-01-25 10:18:53 +0800

    add error case for sts.test

--- test/sts.test.js
+++ test/sts.test.js
@@ -62,6 +62,30 @@ describe('test/sts.test.js', function () {
       assert.equal(result.res.status, 200);
     });
 
+    it('should handle error in assume role', function*() {
+      var stsClient = sts(stsConfig);
+      var policy = `
+      {
+        "Statements": [
+          {
+            "Action": [
+              "oss:*"
+            ],
+            "Effect": "Allow",
+            "Resource": ["acs:oss:*:*:*"]
+          }
+        ],
+        "Version": "1"
+      }`;
+
+      try {
+        yield* stsClient.assumeRole(stsConfig.roleArn, policy);
+        assert(false);
+      } catch (err) {
+        err.message.should.match(/InvalidParameter.PolicyGrammar/);
+      }
+    });
+
     it('should list objects using STS', function* () {
       var stsClient = sts(stsConfig);
       var result = yield* stsClient.assumeRole(stsConfig.roleArn);
