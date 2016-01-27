a9fa79736516aec63be4ccb2834b13c95e7ddaa8
dead_horse <dead_horse@qq.com>
2016-01-27 12:06:22 +0800

    deps: update dependencies
    add pre-release and post-release hook

---
+++ .autod.conf.js
@@ -0,0 +1,17 @@
+module.exports = {
+  write: true,
+  prefix: '~',
+  devprefix: '^',
+  devdep: [
+    'mocha',
+    'autod',
+    'should',
+    'thunk-mocha',
+    'istanbul',
+    'git-pre-hooks'
+  ],
+  exclude: [
+    'dist',
+    'browser.js'
+  ],
+};
