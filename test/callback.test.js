03e3438db3eb07c25e97ddd717c1fc78176c4a4d
jingdan <binghaiwang@gmail.com>
2018-01-26 11:07:03 +0800

    test: change test name

--- test/callback.test.js
+++ test/callback.test.js
@@ -35,7 +35,7 @@ describe('test/multipart.test.js', function () {
     afterEach(mm.restore);
     // callback server on EC2, maybe fail on China, bug pass on travis ci
     // callback server down, skip it
-    it('should parse response with callback', function* () {
+    it('multipart upload should parse response with callback', function* () {
       // create a file with 1M random data
       var fileName = yield utils.createTempFile('upload-with-callback', 1024 * 1024);
 
@@ -54,7 +54,26 @@ describe('test/multipart.test.js', function () {
       assert.equal(result.data.Status, 'OK');
     });
 
-    it('should parse response with callback', function* () {
+    it('multipart upload with no more 100k file should parse response with callback', function* () {
+      // create a file with 1M random data
+      var fileName = yield utils.createTempFile('upload-with-callback', 50 * 1024);
+
+      var name = prefix + 'multipart/upload-with-callback';
+      var result = yield this.store.multipartUpload(name, fileName, {
+        partSize: 100 * 1024,
+        headers: {
+          'x-oss-callback': utils.encodeCallback({
+            url: config.callbackServer,
+            query: {user: 'js-sdk'},
+            body: 'bucket=${bucket}&object=${object}'
+          })
+        }
+      });
+      assert.equal(result.res.status, 200);
+      assert.equal(result.data.Status, 'OK');
+    });
+
+    it('putStream should parse response with callback', function* () {
       var name = prefix + 'ali-sdk/oss/putstream-callback.js';
       var result = yield this.store.putStream(name, fs.createReadStream(__filename), {
         headers: {
@@ -70,7 +89,7 @@ describe('test/multipart.test.js', function () {
       assert.equal(result.data.Status, 'OK');
     });
 
-    it('should parse response with callback', function* () {
+    it('put should parse response with callback', function* () {
       var name = prefix + 'ali-sdk/oss/put-callback.js';
       var result = yield this.store.put(name, __filename, {
         headers: {
