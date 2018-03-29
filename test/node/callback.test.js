df08255667d8c3081e64d46973b3b70578b175c4
jingdan <binghaiwang@gmail.com>
2018-03-29 15:28:31 +0800

    fix(Browser): multipartUpload InvalidPartOrderError issues

--- test/node/callback.test.js
+++ test/node/callback.test.js
@@ -7,7 +7,7 @@ const config = require('../config').oss;
 const { callbackServer } = require('../const');
 const mm = require('mm');
 
-describe('test/multipart.test.js', () => {
+describe('test/callback.test.js', () => {
   const { prefix } = utils;
 
   before(function* () {
