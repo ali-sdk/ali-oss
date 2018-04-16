03c5c6bf11f3d359c3283cd5efc7ab42cea83c66
润生 <sdlcdhch@gmail.com>
2018-04-16 16:54:05 +0800

    test: fix testcases

--- test/browser/browser.test.js
+++ test/browser/browser.test.js
@@ -551,25 +551,25 @@ describe('browser', () => {
     // });
     //
     it('should signature url for PUT', function* () {
-			const putString = 'Hello World';
-			let contentMd5 = crypto1
-				.createHash('md5')
-				.update(new Buffer(putString, 'utf8'))
-				.digest('base64');
-			console.log(contentMd5);
-			const url = this.store.signatureUrl(this.name, {
-				method: 'PUT',
-				'Content-Type': 'text/plain; charset=UTF-8',
-				'Content-Md5': contentMd5,
-			});
-			const headers = {
-				'Content-Type': 'text/plain; charset=UTF-8',
-				'Content-MD5': contentMd5,
-			};
-			const res = yield urllib.request(url, { method: 'PUT', data: putString, headers });
-			assert.equal(res.status, 200);
-			const headRes = yield this.store.head(this.name);
-			assert.equal(headRes.status, 200);
+      const putString = 'Hello World';
+      const contentMd5 = crypto1
+        .createHash('md5')
+        .update(new Buffer(putString, 'utf8'))
+        .digest('base64');
+      console.log(contentMd5);
+      const url = this.store.signatureUrl(this.name, {
+        method: 'PUT',
+        'Content-Type': 'text/plain; charset=UTF-8',
+        'Content-Md5': contentMd5,
+      });
+      const headers = {
+        'Content-Type': 'text/plain; charset=UTF-8',
+        'Content-MD5': contentMd5,
+      };
+      const res = yield urllib.request(url, { method: 'PUT', data: putString, headers });
+      assert.equal(res.status, 200);
+      const headRes = yield this.store.head(this.name);
+      assert.equal(headRes.status, 200);
     });
 
     it('should signature url get need escape object ok', function* () {
