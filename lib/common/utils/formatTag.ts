4b8edf6b9cdc2e46d438847a3894d4a090e56a55
冷若霜寒 <912881342@qq.com>
2020-06-30 11:14:13 +0800

    feat: listBucket support search by tagging (#819)

---
+++ lib/common/utils/formatTag.ts
@@ -0,0 +1,20 @@
+import { isObject } from './isObject';
+
+export function formatTag(obj) {
+  if (obj.Tagging !== undefined) {
+    obj = obj.Tagging.TagSet.Tag;
+  } else if (obj.TagSet !== undefined) {
+    obj = obj.TagSet.Tag;
+  } else if (obj.Tag !== undefined) {
+    obj = obj.Tag;
+  }
+
+  obj = obj && isObject(obj) ? [obj] : obj || [];
+
+  const tag = {};
+  obj.forEach((item) => {
+    tag[item.Key] = item.Value;
+  });
+
+  return tag;
+}
