a5c7efafd65d796b205871e4e5a3852cc8b50d6c
biejia <wb-bj559483@alibaba-inc.com>
2020-07-29 16:16:43 +0800

    feat: add encoder types

--- lib/common/utils/encoder.ts
+++ lib/common/utils/encoder.ts
@@ -1,4 +1,6 @@
-export function encoder(str: string, encoding = 'utf-8') {
+import { THeaderEncoding } from '../../types/experimental';
+
+export function encoder(str: string, encoding: THeaderEncoding = 'utf-8') {
   if (encoding === 'utf-8') return str;
   return Buffer.from(str).toString('latin1');
 }
