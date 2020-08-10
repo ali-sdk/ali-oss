57b87f2f6c867958f5263b81e238414142005f39
weiyie <912881342@qq.com>
2020-08-10 13:59:48 +0800

    fix: sts

--- src/node/sts/index.ts
+++ src/node/sts/index.ts
@@ -147,7 +147,12 @@ class STS {
   }
 
   _escape(str) {
-    return encodeURIComponent(str).replace(/\*/g, '%2A');
+    return encodeURIComponent(str)
+      .replace(/!/g, '%21')
+      .replace(/'/g, '%27')
+      .replace(/\(/g, '%28')
+      .replace(/\)/g, '%29')
+      .replace(/\*/g, '%2A');
   }
 }
 
