9f0f975b536a4ab05a9fec99195b0857a03cbbd3
weiyie <912881342@qq.com>
2020-09-24 15:19:36 +0800

    test: add auto set sts test case

--- lib/common/utils/setSTSToken.ts
+++ lib/common/utils/setSTSToken.ts
@@ -7,5 +7,16 @@ export async function setSTSToken(this: any) {
   if (credentials.securityToken) {
     credentials.stsToken = credentials.securityToken;
   }
+  checkCredentials(credentials);
   Object.assign(this.options, credentials);
+}
+
+function checkCredentials(obj) {
+  const stsTokenKey = ['accessKeySecret', 'accessKeyId', 'stsToken'];
+  const objKeys = Object.keys(obj);
+  stsTokenKey.forEach(_ => {
+    if (!objKeys.find(key => key === _)) {
+      throw Error(`refreshSTSToken must return contains ${_}`);
+    }
+  });
 }
