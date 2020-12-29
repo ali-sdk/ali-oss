e290aab4249f4259bc8aef495c0fb107763bc73d
beajer <919060679@qq.com>
2020-12-29 10:15:13 +0800

    fix: do not copy buffer in completeMultipartUpload

--- test/node/utils/deepCopy.test.js
+++ test/node/utils/deepCopy.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const { Buffer } = require('buffer');
-const { deepCopy } = require('../../../lib/common/utils/deepCopy');
+const { deepCopy, deepCopyWith } = require('../../../lib/common/utils/deepCopy');
+const { isBuffer } = require('../../../lib/common/utils/isBuffer');
 
 describe('utils/deepCopy()', () => {
   it('should copy big Buffers correctly', () => {
@@ -8,8 +9,33 @@ describe('utils/deepCopy()', () => {
     // See https://nodejs.org/api/buffer.html#buffer_buffer_constants_max_length
     const numberBytes = Math.pow(2, 30) - 1;
     const obj = {
-      buffer: Buffer.alloc(numberBytes),
+      buffer: Buffer.alloc(numberBytes)
     };
     assert.deepStrictEqual(deepCopy(obj), obj);
+  });
+
+  it('should skip some properties when use deepCopyWith', () => {
+    const numberBytes = Math.pow(2, 30) - 1;
+    const obj = {
+      a: 1,
+      b: {
+        c: 2
+      },
+      buffer: Buffer.alloc(numberBytes)
+    };
+    const copy1 = deepCopyWith(obj, (_, key) => {
+      if (key === 'buffer') return null;
+    });
+    const copy2 = deepCopyWith(obj, _ => {
+      if (isBuffer(_)) return null;
+    });
+    assert.deepStrictEqual(copy1, {
+      a: 1,
+      b: {
+        c: 2
+      },
+      buffer: null
+    });
+    assert.deepStrictEqual(copy1, copy2);
   });
 });
