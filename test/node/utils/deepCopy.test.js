4093def547a84c52a1886faa680f12afcc58d183
beajer <919060679@qq.com>
2020-12-31 14:24:44 +0800

    fix: test case of buffer replication in Nodejs8

--- test/node/utils/deepCopy.test.js
+++ test/node/utils/deepCopy.test.js
@@ -4,14 +4,16 @@ const { deepCopy, deepCopyWith } = require('../../../lib/common/utils/deepCopy')
 const { isBuffer } = require('../../../lib/common/utils/isBuffer');
 
 describe('utils/deepCopy()', () => {
-  it('should copy big Buffers correctly', () => {
+  it.only('should copy big Buffers correctly', () => {
     // 2^30 - 1 ~ 1GB is max size on 32-bit computer
     // See https://nodejs.org/api/buffer.html#buffer_buffer_constants_max_length
     const numberBytes = Math.pow(2, 30) - 1;
     const obj = {
       buffer: Buffer.alloc(numberBytes)
     };
-    assert.deepStrictEqual(deepCopy(obj), obj);
+    const copy = deepCopy(obj);
+    assert.strictEqual(Object.keys(obj).length, Object.keys(copy).length);
+    assert(obj.buffer.equals(copy.buffer));
   });
 
   it('should skip some properties when use deepCopyWith', () => {
