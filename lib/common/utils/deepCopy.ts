c18f4ea97bf3edc485d67f82783a737b285fcaeb
beajer <919060679@qq.com>
2021-01-04 01:18:42 -0600

    fix: do not copy buffer in completeMultipartUpload (#915)
    * fix: do not copy buffer in completeMultipartUpload
    
    * fix: test case because Image Process updated compression algorithm
    
    * fix: test case of buffer replication in Nodejs8
    
    * fix: remove test.only
    
    * fix: codecov

--- lib/common/utils/deepCopy.ts
+++ lib/common/utils/deepCopy.ts
@@ -16,3 +16,33 @@ export const deepCopy = (obj) => {
 
   return copy;
 };
+
+export const deepCopyWith = (obj: any, customizer?: (v: any, k: string, o: any) => any) => {
+  function deepCopyWithHelper(value: any, innerKey: string, innerObject: any) {
+    const result = customizer!(value, innerKey, innerObject);
+    if (result !== undefined) return result;
+
+    if (value === null || typeof value !== 'object') {
+      return value;
+    }
+
+    if (isBuffer(value)) {
+      return obj.slice();
+    }
+
+    const copy = Array.isArray(value) ? [] : {};
+
+    Object.keys(value).forEach((k) => {
+      copy[k] = deepCopyWithHelper(value[k], k, value);
+    });
+
+    return copy;
+  }
+
+  if (customizer) {
+    return deepCopyWithHelper(obj, '', null);
+  } else {
+    return deepCopy(obj);
+  }
+
+};
