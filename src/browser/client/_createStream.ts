31d81b509634351a55c0926257d4589528efbb86
taotao7 <moca_tao7@foxmail.com>
2021-12-13 13:41:50 +0800

    fix: use arrayBuffer first, fall back to FileReader if it does not work #943

--- src/browser/client/_createStream.ts
+++ src/browser/client/_createStream.ts
@@ -2,14 +2,25 @@ import { isBlob } from '../../common/utils/isBlob';
 import { isFile } from '../../common/utils/isFile';
 import { isBuffer } from '../../common/utils/isBuffer';
 
-export async function _createStream(
-  file: any,
-  start: number,
-  end: number,
-): Promise<Buffer> {
+const getBuffer = file => {
+  // Some browsers do not support Blob.prototype.arrayBuffer, such as IE
+  if (file.arrayBuffer) return file.arrayBuffer();
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      resolve(e.target?.result);
+    };
+    reader.onerror = (e) => {
+      reject(e);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+};
+
+export async function _createStream(file: any, start: number, end: number): Promise<Buffer> {
   if (isBlob(file) || isFile(file)) {
     const _file = file.slice(start, end);
-    const fileContent = await _file.arrayBuffer();
+    const fileContent = await getBuffer(_file);
     return Buffer.from(fileContent);
   } else if (isBuffer(file)) {
     return file.subarray(start, end);
