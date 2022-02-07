e2f9f6882f68591bc20c345068597b1058cbe1b0
taotao7 <moca_tao7@foxmail.com>
2022-02-07 15:54:44 +0800

    chore: some test error and variable

--- test/node/cleanAllBucket.js
+++ test/node/cleanAllBucket.js
@@ -3,4 +3,27 @@ const config = require('../config').oss;
 const OSS = require('../..');
 
 const store = new OSS(config);
-utils.cleanAllBucket(store);
+
+store.listBuckets().then(r => {
+  const bucketList = [];
+  r.buckets.forEach(i => {
+    if (i.name.indexOf('ali-oss') === 0) {
+      bucketList.push({
+        bucket: i.name,
+        region: i.region
+      });
+    }
+  });
+
+  for (const bucketListItem of bucketList) {
+    const client = new OSS({
+      ...store.options,
+      bucket: bucketListItem.bucket,
+      region: bucketListItem.region
+    });
+    utils.cleanBucket(client, bucketListItem.bucket).catch(e => {
+      console.log('bucket name =======>', bucketListItem.bucket);
+      console.log('error:====>', e);
+    });
+  }
+});
