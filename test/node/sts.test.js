7186711c6e9e795f530bd39716db45a78580810e
taotao7 <moca_tao7@foxmail.com>
2022-04-29 10:40:23 +0800

    chore: optimized test case

--- test/node/sts.test.js
+++ test/node/sts.test.js
@@ -202,16 +202,7 @@ describe('test/sts.test.js', () => {
 
     it('asyncSignatureUrl will should use refreshSTSToken', async () => {
       const { credentials } = await stsClient.assumeRole(stsConfig.roleArn);
-      const stsService = () => {
-        return new Promise((resolve, reject) => {
-          resolve({
-            accessKeyId: 'b',
-            accessKeySecret: 'b',
-            stsToken: 'b'
-          });
-          if (1 === 2) reject();
-        });
-      };
+      let flag = false;
 
       store = new OSS({
         region: config.region,
@@ -219,29 +210,15 @@ describe('test/sts.test.js', () => {
         accessKeySecret: credentials.AccessKeySecret,
         stsToken: credentials.SecurityToken,
         refreshSTSToken: async () => {
-          const info = await stsService();
-          return {
-            accessKeyId: info.accessKeyId,
-            accessKeySecret: info.accessKeySecret,
-            stsToken: info.stsToken
-          };
+          flag = true;
         },
         bucket: stsConfig.bucket,
         refreshSTSTokenInterval: 1000
       });
       await utils.sleep(2000);
-      const a = await store.asyncSignatureUrl('test.txt');
+      await store.asyncSignatureUrl('test.txt');
 
-      store = new OSS({
-        region: config.region,
-        accessKeyId: 'b',
-        accessKeySecret: 'b',
-        stsToken: 'b',
-        bucket: stsConfig.bucket
-      });
-
-      const b = store.signatureUrl('test.txt');
-      assert.equal(a, b);
+      assert(flag);
     });
   });
 });
