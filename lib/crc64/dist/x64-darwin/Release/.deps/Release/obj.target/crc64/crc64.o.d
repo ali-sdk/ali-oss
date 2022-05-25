aa6a49b91bde4f1c66fce617decd1a5f6c756c8e
taotao7 <moca_tao7@foxmail.com>
2022-05-25 09:27:38 +0800

    feat: add x64-drawin

---
+++ lib/crc64/dist/x64-darwin/Release/.deps/Release/obj.target/crc64/crc64.o.d
@@ -0,0 +1,29 @@
+cmd_Release/obj.target/crc64/crc64.o := c++ -o Release/obj.target/crc64/crc64.o ../crc64.cpp '-DNODE_GYP_MODULE_NAME=crc64' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/gang/Library/Caches/node-gyp/16.3.0/include/node -I/Users/gang/Library/Caches/node-gyp/16.3.0/src -I/Users/gang/Library/Caches/node-gyp/16.3.0/deps/openssl/config -I/Users/gang/Library/Caches/node-gyp/16.3.0/deps/openssl/openssl/include -I/Users/gang/Library/Caches/node-gyp/16.3.0/deps/uv/include -I/Users/gang/Library/Caches/node-gyp/16.3.0/deps/zlib -I/Users/gang/Library/Caches/node-gyp/16.3.0/deps/v8/include  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++14 -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/crc64/crc64.o.d.raw   -c
+Release/obj.target/crc64/crc64.o: ../crc64.cpp \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/cppgc/common.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8config.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8-internal.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8-version.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8-platform.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node_version.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node_api.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/js_native_api.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/js_native_api_types.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node_api_types.h \
+  /Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node_buffer.h
+../crc64.cpp:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/cppgc/common.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8config.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8-internal.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8-version.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/v8-platform.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node_version.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node_api.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/js_native_api.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/js_native_api_types.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node_api_types.h:
+/Users/gang/Library/Caches/node-gyp/16.3.0/include/node/node_buffer.h:
