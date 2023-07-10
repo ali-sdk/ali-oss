8c38aac56cc3c7c195d445befc4eb016136579f8
csg01123119 <csg01123119@alibaba-inc.com>
2023-07-10 15:38:57 +0800

    fix: test case

--- test/node/cleanAllBucket.js
+++ test/node/cleanAllBucket.js
@@ -2,6 +2,8 @@ const { cleanAllBucket } = require('./utils');
 const { oss: config } = require('../config');
 const OSS = require('../..');
 
-const store = new OSS(config);
+// eslint-disable-next-line no-console
+console.log('cleanAllBucket...');
 
+const store = new OSS(config);
 cleanAllBucket(store, 50, true);
