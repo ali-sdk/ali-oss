74d6160eaa4ebc01a7c92a477cc51c6b8b590ff3
csg01123119 <csg01123119@alibaba-inc.com>
2023-07-18 11:14:08 +0800

    fix: optimization check for code containing ak

--- task/detect-secrets.js
+++ task/detect-secrets.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 const files = process.argv.slice(2);
-const reg = /['"]LT([A-Za-z0-9+/=]{14}|LT[A-Za-z0-9+/=]{22}|LT[A-Za-z0-9+/=]{28})['"]/;
-files.forEach((val, index) => {
+const reg = /['"]([0-9a-z+/=]{16}|[0-9a-z+/=]{24}|[0-9a-z+/=]{30})['"]/gi;
+files.forEach(val => {
   try {
     const data = fs.readFileSync(val, 'utf8');
-    if (reg.test(data)) {
+    const res = data.match(reg);
+    if (res && res.length > 0) {
       console.error("Don't push accessKeyId/accessKeySecret to repo! ------ File: " + val);
+      res.forEach(item => console.log(item));
       process.exit(-1);
     }
   } catch (err) {
-    console.error('file error ----' + val);
+    console.error('check AK file error ----' + val);
     process.exit(-1);
   }
 });
