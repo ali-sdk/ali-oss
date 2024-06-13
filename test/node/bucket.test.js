8dbcf61c44e80c59adc8476464d33f8282c0c718
csg01123119 <csg01123119@alibaba-inc.com>
2024-06-13 10:52:07 +0800

    feat: support list buckets headers

--- test/node/bucket.test.js
+++ test/node/bucket.test.js
@@ -267,7 +267,7 @@ describe('test/bucket.test.js', () => {
 
         it('should list buckets by group id', async () => {
           const { buckets } = await store.listBuckets({}, { headers: { 'x-oss-resource-group-id': 'rg-testid' } });
-          assert(buckets === null || buckets.length > -1);
+          assert(buckets === null);
         });
 
         after(async () => {
